522403f5741b9eee648a9c222b613742f42aa10a
scott <lsy@gmail.com>
2025-02-16 15:31:23 -0500

    dfs

--- Assignment_3/sbp.py
+++ Assignment_3/sbp.py
@@ -59,23 +59,23 @@ class Sbp:
         for x, y in cells:
             new_x, new_y = x + dx, y + dy
 
-            # Check if the new position is within the board boundaries
-            if not (0 <= new_x < self.width and 0 <= new_y < self.height):
-                return False
+            # if not (0 <= new_x < self.width and 0 <= new_y < self.height):
+            #     return False
 
             target_cell = self.board[new_y][new_x]
 
-            # Allow movement into empty cells (0) or goal cells (-1) for the master brick (2)
             if target_cell == 0:
                 continue
-            if target_cell == -1 and piece == 2:
-                continue
-
-            # Disallow movement into walls (1) or other pieces
-            if target_cell == 1 or (target_cell not in [0, -1] and (new_x, new_y) not in cells):
+            if target_cell == 1:
                 return False
 
+            if target_cell == -1 and piece != 2:
+                return False
+
+            if target_cell not in [0 - 1] and (new_x, new_y) not in cells:
+                return False
         return True
+
     def available_moves(self):
         """Gets all available moves."""
         moves = []
